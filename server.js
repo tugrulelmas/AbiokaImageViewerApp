2374321439215f749f0c18d6c4180dce213dc2c8
tugrulelmas <tugrulelmas@gmail.com>
2016-08-24 21:55:09 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 var express = require('express');
 var http = require('http');
+var https = require('https');
 var app = express();
 
 app.get("/image", function (request, response) {
   var url = request.query.url;
   
-  http.get(url, function(res) {
+  var client = getRequestClient(url);
+  client.get(url, function(res) {
     var contentType = res.headers['content-type'];
     if(contentType.indexOf('image/') === -1){
       response.statusCode = 400
@@ -28,6 +30,13 @@ app.get("/image", function (request, response) {
     response.end(e.message);
   });
 });
+
+function getRequestClient(url){
+  if(url.indexOf('https://') === 0)
+    return https;
+  else
+    return http;
+}
 
 var listener = app.listen(process.env.PORT, function () {
   console.log('Your app is listening on port ' + listener.address().port);
